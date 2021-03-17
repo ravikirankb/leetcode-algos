76820c53237a459539760ab1691088e7418b85aa
Ravikiran <ravikirankb@gmail.com>
2021-03-18 00:02:58 +0530

    Update decompress-runlength.py

--- decompress-runlength.py
+++ decompress-runlength.py
@@ -1,6 +1,9 @@
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
-        result=[]
-        for i in range(0,len(nums),2): # range has start, stop, skip
-            result += [nums[i+1]] * nums[i] # oohoo can use * to repeat the element n times
+        result = []
+        for i in range(0,len(nums),2):
+            #print(i)
+            for j in range(0,nums[i]):
+                result.append(nums[i + 1])
+                
         return result
