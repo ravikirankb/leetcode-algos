bd121356ed34b88f4ee69f17768999563659923b
Ravikiran <ravikirankb@gmail.com>
2021-03-20 23:35:56 +0530

    Update crawler-log-folder.py

--- crawler-log-folder.py
+++ crawler-log-folder.py
@@ -12,3 +12,19 @@ class Solution:
                 
                 
         return len(stack)
+
+    
+   
+# memory efficient
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        steps =0;
+        for log in logs:
+            if log == "../":
+                if steps > 0:
+                   steps -= 1
+            elif log == "./":
+                continue
+            else:
+                steps += 1
+        return steps
