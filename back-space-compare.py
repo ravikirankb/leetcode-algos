e37c059dbeecaf14565f5cdcd5eae56b2933d25b
Ravikiran <ravikirankb@gmail.com>
2021-03-21 19:57:58 +0530

    Update back-space-compare.py

--- back-space-compare.py
+++ back-space-compare.py
@@ -16,3 +16,29 @@ class Solution:
                 res2.append(t)
         
         return ','.join(res1) == ','.join(res2)
+
+    
+    ## slightly memory efficient
+    
+    class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        res1,res2,len1,len2 = '','',0,0
+        for i,s in enumerate(S):
+            if s == '#':
+                if len1>0:
+                 res1 = res1[0:len1-1]                
+                 len1 -= 1
+            else:
+                res1 += s
+                len1 += 1
+        
+        for j,t in enumerate(T):
+            if t == '#':
+                if len2 >0:
+                 res2 = res2[0:len2-1]                
+                 len2 -= 1
+            else:
+                res2 += t
+                len2 += 1
+                
+        return res1 == res2
