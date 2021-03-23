57e8338379c892e8b6a026920e036090e6b97537
Ravikiran <ravikirankb@gmail.com>
2021-03-23 14:48:28 +0530

    Update consistentstrings.py

--- consistentstrings.py
+++ consistentstrings.py
@@ -1,17 +1,14 @@
+# fast and memory efficient
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         chars = set(allowed)
-        count,result = 0,len(words)
+        result = len(words)
         
         for word in words:
             for ch in word:
                 if ch not in chars:
-                    count += 1
-                    
-            if count > 0:
-               result -= 1
-            
-            count = 0
+                    result -=1
+                    break
                     
         return result
         
