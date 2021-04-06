41cb28faaa018914360f8b17940a61c3a31ad912
Ravikiran <ravikirankb@gmail.com>
2021-04-06 08:41:08 -0700

    Update sum-of-left-leaves.py

--- sum-of-left-leaves.py
+++ sum-of-left-leaves.py
@@ -1,19 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+    
+### simple recursive solution
+
 class Solution:
-     def sumOfLeftLeaves(self, root: TreeNode) -> int:
-        
-        def dfs(root,isLeaf):
-            if not root:return 0
-            if isLeaf and not root.left and not root.right:
-                return root.val
-                
-            return dfs(root.left,1) + dfs(root.right,0)
-            
-        return dfs(root,0)
+     def sumOfLeftLeaves(self, root: TreeNode,isLeft = False) -> int:
+        if not root:
+            return 0
+        elif isLeft and root and not root.left and not root.right:
+            return root.val
+        else:
+            return self.sumOfLeftLeaves(root.left,True) + self.sumOfLeftLeaves(root.right,False)
             
          
