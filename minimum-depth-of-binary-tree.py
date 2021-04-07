b1b48dcfebbe055bc86186c21015c41b3b45c951
Ravikiran <ravikirankb@gmail.com>
2021-04-07 09:54:31 -0700

    Create minimum-depth-of-binary-tree.py

---
+++ minimum-depth-of-binary-tree.py
@@ -0,0 +1,25 @@
+## memory efficient solution
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        que = [(root,1)]
+        min_depth = float("inf")
+        while que:
+            cur_node,depth = que.pop(0)
+            
+            if cur_node and not cur_node.left and not cur_node.right:
+              ## since the first found leaf is the minimum depth stop the loop and do not continue looking further nodes.
+               min_depth = min(min_depth,depth)
+               break
+              
+            if cur_node.left:
+              que.append((cur_node.left,depth + 1))
+            if cur_node.right:
+              que.append((cur_node.right,depth + 1))
+            
+            
+        return min_depth
+            
