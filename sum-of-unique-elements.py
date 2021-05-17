a85186310190b73e75962a35ac780114c60886e9
Ravikiran <ravikirankb@gmail.com>
2021-05-17 11:52:19 +0530

    Update sum-of-unique-elements.py

--- sum-of-unique-elements.py
+++ sum-of-unique-elements.py
@@ -13,3 +13,22 @@ class Solution:
                 res += k
                 
         return res
+
+    ## with two sets- slightly less efficient
+    class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        add_set = set()
+        remove_set = set()
+        
+        res = 0
+        for n in nums:
+            if n in add_set and n in remove_set:
+               add_set.remove(n)
+               res -= n
+            elif n not in add_set and n not in remove_set:
+               add_set.add(n)
+               res +=n
+            
+            remove_set.add(n)
+            
+        return res
