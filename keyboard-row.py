d857fa2438dd15d28c8dbacd67150845631e245f
Ravikiran <ravikirankb@gmail.com>
2021-05-27 11:49:01 +0530

    Update keyboard-row.py

--- keyboard-row.py
+++ keyboard-row.py
@@ -39,5 +39,39 @@ class Solution:
                 
                 
         return result
+    
+    
+    ## slightly faster solution, avoids extra loops
+    class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        t = set({'p', 'o', 'y', 'q', 'r', 'u', 'e', 'w', 't', 'i', 'P', 'O', 'Y', 'Q', 'R', 'U', 'E', 'W', 'T', 'I'})
+        m = set({'h', 'a', 's', 'f', 'j', 'l', 'g', 'd', 'k', 'H', 'A', 'S', 'F', 'J', 'L', 'G', 'D', 'K'})
+        b = set({'m', 'z', 'b', 'n', 'v', 'x', 'c', 'M', 'Z', 'B', 'N', 'V', 'X', 'C'})
+       
+        foundin_t,foundin_m,found_b = True,True,True
+        result = []
+        for word in words:
+            for w in word:
+                if w not in t:
+                   foundin_t = False
+                   pass
+                    
+                if w not in m:
+                    foundin_m = False
+                    pass
+                
+                if w not in b:
+                    foundin_b = False
+                    pass
+            
+                
+            if foundin_t or foundin_m or foundin_b:
+                result.append(word)
+            
+            foundin_t,foundin_m,foundin_b = True,True,True
+                
+        return result
+                
+                
                 
                 
