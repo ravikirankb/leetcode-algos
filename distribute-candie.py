03db5f9a16dafc33c7da66c2f451fef553970d58
Ravikiran <ravikirankb@gmail.com>
2021-05-27 16:48:04 +0530

    Update distribute-candie.py

--- distribute-candie.py
+++ distribute-candie.py
@@ -2,7 +2,6 @@ class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
          l = len(set(candyType))
          
-         cl = len(candyType)
-         tl = cl // 2
+         tl = len(candyType) //2
         
          return tl if tl<=l else l
